f5a346f259d987933793c261b6ba69e19f507105
futszak <futszak@gmail.com>
2017-07-12 20:28:12 +0200

    first test commit

---
+++ communicatorconfig.py
@@ -0,0 +1,18 @@
+config = {
+    'HOST': "x.x.x.x",
+    'PORT': "11568",
+    'BUFSIZ': 1024,
+    'MAX_CONNECTION': 100,
+    'debug': True,
+    'UDP_IP': "x.x.x.x",
+    'UDP_PORT': 514,
+    'PROCNAME': "gps-datareceiver",
+    'DB_HOSTNAME': "localhost",
+    'DB_USERNAME': "db_username",
+    'DB_PASSWORD': "db_password",
+    'DB_DBNAME': "db_name",
+    'DB_TABLENAME': "gps",
+    'WEBtimeout': 60,
+    'googlekey': "google api key",
+    'WEBtesttimeout': 5
+    }
---
+++ senderconfig.py
@@ -0,0 +1,20 @@
+config = {
+    'HOST': 'x.x.x.x',
+    'PORT': 11568,
+    'BUFSIZ': 1024,
+    'TMOUT': 1,
+    'RESTART': 0,
+    'MYHOSTNAME': 'my_hostname',
+    'MYHOSTPASS': 'my_password',
+    'PROCNAME': "gps-sender",
+    'UDP_IP': "x.x.x.x",
+    'UDP_PORT': 514,
+    'jsonlengt': 10,
+    'savefilename': 'saved',
+    'trytorestart': 2,
+    'debug': False,
+    'GPSFAKEDATA': True,
+    'alive': 60,
+    'nextgps': 5,
+    'waitingforserveranswer': 20
+}
