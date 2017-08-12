8bf3bf1b3e2e7b1feca981a551dec78615d92a9f
futszaK <futszak@gmail.com>
2017-08-12 15:06:31 +0200

    new fields - login on web

--- web/config.php
+++ web/config.php
@@ -1,8 +1,8 @@
 <?php
-// config database
-$db_host = ("hostname");
-$db_user = ("username");
-$db_pass = ("password");
+// config
+$db_host = ("localhost");
+$db_user = ("communicator");
+$db_pass = ("gr0m");
 $db_name = ("communicator");
 $db_table = ("gps");
 $db_states = ("states");
