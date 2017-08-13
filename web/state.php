c4e54084f3b207ffa22a847d433dfdee8ca688b1
futszaK <futszak@gmail.com>
2017-08-13 12:51:06 +0200

    in message sending information about last command

--- web/state.php
+++ web/state.php
@@ -1,10 +1,14 @@
 <?php
 include('./config.php');
+
 class state {
+
   var $handle;
+
   function __construct($db_host,$db_user,$db_pass,$db_name) {
     $this->handle = mysqli_connect($db_host,$db_user,$db_pass,$db_name) or die('bad data to base');
   }
+
   function circuit() {
     $q = mysqli_query($this->handle,'SELECT state FROM gps ORDER BY time DESC LIMIT 1;');
     $a = mysqli_fetch_assoc($q); $a = $a['state'];
@@ -15,6 +19,7 @@ class state {
     if ($action == "on" || $action == "off" || $action == "restart" || $action == "reset")
       { mysqli_query($this->handle,"INSERT INTO commands (action) VALUES ('".$action."');"); }
     }
+
   function message() {
       $q = mysqli_query($this->handle,'SELECT action FROM commands ORDER BY time DESC LIMIT 1;');
       $a = mysqli_fetch_assoc($q); $a = $a['action'];
@@ -26,8 +31,13 @@ class state {
       $a = mysqli_fetch_assoc($w);
       $webtime = 60; // to removed
       $a = $a['webtime'];
-      if ((time()-$a) > $webtime) { return("no"); } else { return("ok"); }  }
+      if ((time()-$a) > $webtime) { return("no"); } else {
+      $q = 'SELECT time,action FROM commands WHERE (action="on" or action="off") ORDER BY time DESC LIMIT 1';
+      $w = mysqli_query($this->handle,$q);
+      $a = mysqli_fetch_assoc($w);
+      return($a['action']); }  }
   }
+
   function refresh() {
     $a = time();
     mysqli_query($this->handle,'UPDATE livestream SET webtime = '.$a.' LIMIT 1');
