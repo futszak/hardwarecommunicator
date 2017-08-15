e743168b80ca62c41ec7d285a2090be537f8b5dd
futszaK <futszak@gmail.com>
2017-08-15 15:17:15 +0200

    new functionality
    user can choice how long is table

--- web/history.php
+++ web/history.php
@@ -19,12 +19,12 @@
                   <div class="row">
                     <div class="col-lg-3 text-center">
                           <div class="alert alert-info">
-                              <a href="today"><strong>Dzisiejsza</strong> historia auta</a>
+                              <a href="history.php?render=1000"><strong>Ostatni tysiąc</strong> minut</a>
                           </div>
                     </div>
                     <div class="col-lg-3 text-center">
                           <div class="alert alert-info">
-                              <a href="month"><strong>Cała</strong> historia auta</a>
+                              <a href="history.php?render=10000"><strong>Ostatnie 10 tysięcy</strong> minut</a>
                           </div>
                     </div>
                     <div class="col-lg-6 text-center">
@@ -47,6 +47,7 @@
 
   <table style="width:100%">
   <tr>
+  <th>Lp</th>
   <th>Data i godzina</th>
   <th>Wspolrzedne geograficzne</th>
   <th>Stan obwodu</th>
@@ -56,9 +57,8 @@
 if (file_exists('config.php')) { include('./config.php'); } else { die("no config file"); }
 if (file_exists('config.php')) { include('./state.php'); } else { die("no state.php file"); }
 $lz = new state($db_host,$db_user,$db_pass,$db_name);
-$prt = ($lz->tblrender());
-echo($prt);
-
+if ($_GET['render']) { $prt = ($lz->tblrender($_GET['render'])); } else {
+$prt = ($lz->tblrender()); }
  ?>
 
   </table></body>
