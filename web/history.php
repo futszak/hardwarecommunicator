355aa0a7d4e4a412808368c53a5d6549943d6dbb
futszaK <futszak@gmail.com>
2017-08-29 12:34:20 +0200

    header correction

--- web/history.php
+++ web/history.php
@@ -11,12 +11,16 @@
   </style>
   </head>
   <body>
-
       <div id="wrapper">
           <div id="page-wrapper">
               <div class="container-fluid">
                   <!-- /.row -->
                   <div class="row">
+                    <div class="col-lg-6 text-center">
+                      <div class="alert alert-info">
+                        <a href="/"><strong>Powrót</strong> do panelu sterowania</a>
+                      </div>
+                    </div>
                     <div class="col-lg-3 text-center">
                           <div class="alert alert-info">
                               <a href="history.php?render=1000"><strong>Ostatni tysiąc</strong> minut</a>
@@ -27,22 +31,10 @@
                               <a href="history.php?render=10000"><strong>Ostatnie 10 tysięcy</strong> minut</a>
                           </div>
                     </div>
-                    <div class="col-lg-6 text-center">
-                          <div class="alert alert-info">
-                              <a href="/"><strong>Powrót</strong> do panelu sterowania</a>
-                          </div>
-                    </div>
-                      <!-- /.between components -->
-                  <!-- /.row -->
               </div>
-              <!-- /.container-fluid -->
           </div>
-          <!-- /#page-wrapper -->
       </div>
-      <!-- /#wrapper -->
-      <!-- jQuery -->
       <script src="js/jquery.js"></script>
-      <!-- Bootstrap Core JavaScript -->
       <script src="js/bootstrap.min.js"></script>
 
     <table style="width:100%">
