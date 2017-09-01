963a1156537115f60fc2fef5740833ec7697adcf
futszaK <futszak@gmail.com>
2017-09-01 11:32:31 +0200

    refresh last visit if called this function

--- web/jsout.php
+++ web/jsout.php
@@ -2,5 +2,6 @@
 if (file_exists('config.php')) { include('./config.php'); } else { die("no config file"); }
 if (file_exists('state.php')) { include('./state.php'); } else { die("no state.php file"); }
 $lz = new state($db_host,$db_user,$db_pass,$db_name);
+$lz->refresh();
 echo json_encode($lz->lastposition());
 ?>
